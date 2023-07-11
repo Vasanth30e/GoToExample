a3c43b5eacc173d4043757ae4303e15d4528e2dc
unknown <vasanthgamer2000@gmail.com>
2023-07-11 12:46:03 +0530

    Added date methods

--- Program.cs
+++ Program.cs
@@ -25,17 +25,28 @@ namespace GoToExample
             //    Console.WriteLine("Invalid User Name");
             //    goto Again;
             //}
-            string myString = "Vasanth";
-            StringBuilder stringBuilder = new StringBuilder("Welcome ");
-            stringBuilder.Append("To C# ");
+            //string myString = "Vasanth";
+            //StringBuilder stringBuilder = new StringBuilder("Welcome ");
+            //stringBuilder.Append("To C# ");
 
-            stringBuilder.Append("I am String Builder");
+            //stringBuilder.Append("I am String Builder");
 
-            stringBuilder.Append("string: " + myString);
+            //stringBuilder.Append("string: " + myString);
 
-            Console.WriteLine(stringBuilder);
+            //Console.WriteLine(stringBuilder);
+
+            DateTime matchDate = new DateTime(day: 12, month: 08, year: 2025);
+            DateTime currentDate = DateTime.Now;
+            TimeSpan ts = matchDate.Subtract(currentDate);
+            Console.WriteLine("Days Remain" +ts.Days);
+            Console.WriteLine("Days Remain" + ts.Hours);
+            Console.WriteLine(ts.ToString());
 
             Console.ReadKey();
+
+
+
+
 
         }
     }
